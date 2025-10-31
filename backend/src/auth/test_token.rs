447182b51f7745cd833967bb4fcb136a3decb3a2
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2025-10-31 23:43:18 +0100

    fix: remove cookie

--- backend/src/auth/test_token.rs
+++ backend/src/auth/test_token.rs
@@ -1,18 +1,24 @@
 use axum::{Json, Router, routing::get};
-use axum_extra::extract::{CookieJar, cookie::Cookie};
+use axum_extra::extract::CookieJar;
 use tracing::instrument;
 
-use crate::auth::jwt_auth::{COOKIE_NAME, JwtAuth};
+use crate::auth::{
+  jwt_auth::{COOKIE_NAME, JwtAuth},
+  jwt_state::JwtState,
+};
 
 pub fn router() -> Router {
   Router::new().route("/test_token", get(test_token))
 }
 
-#[instrument(skip(cookies))]
-async fn test_token(auth: Option<JwtAuth>, mut cookies: CookieJar) -> (CookieJar, Json<bool>) {
+#[instrument(skip(cookies, jwt))]
+async fn test_token(
+  auth: Option<JwtAuth>,
+  mut cookies: CookieJar,
+  jwt: JwtState,
+) -> (CookieJar, Json<bool>) {
   if auth.is_none() {
-    let cookie = Cookie::build((COOKIE_NAME, "")).path("/").build();
-    cookies = cookies.remove(cookie);
+    cookies = cookies.remove(jwt.create_cookie(COOKIE_NAME, String::new()));
 
     (cookies, Json(false))
   } else {
