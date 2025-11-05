612332a05043ff50bd1f5526c85bdfc026e827c9
ProfiiDev <92174452+Profiidev@users.noreply.github.com>
2025-11-05 22:31:35 +0100

    feat: added postgres list and delete page

--- frontend/src/routes/deployments/postgres/+page.svelte
+++ frontend/src/routes/deployments/postgres/+page.svelte
@@ -1,15 +1,18 @@
 <script lang="ts">
   import {
-    create_deployment,
     delete_deployment,
     list_deployments,
     type DeploymentInfo
   } from '$lib/backend/postgres.svelte';
   import { Plus } from '@lucide/svelte';
-  import { Button } from 'positron-components/components/ui';
+  import { FormDialog } from 'positron-components/components/form';
+  import { Button, ScrollArea } from 'positron-components/components/ui';
+  import { deleteDeployment } from './schema.svelte';
 
-  let name = $state('');
   let deployments: DeploymentInfo[] = $state([]);
+  let deleteOpen = $state(false);
+  let toDeleteName = $state('');
+  let toDeleteUUID = $state('');
 
   const reload = () => {
     list_deployments().then((data) => {
@@ -19,32 +22,62 @@
   };
   reload();
 
-  const create = async () => {
-    await create_deployment({
-      name,
-      storage_mb: 1024
-    });
-
-    reload();
-  };
-
-  const remove = async (uuid: string) => {
-    await delete_deployment(uuid);
-    reload();
+  const deleteConfirm = async () => {
+    if (toDeleteUUID) {
+      let res = await delete_deployment(toDeleteUUID);
+      if (res) {
+        return { error: 'Error deleting deployment.' };
+      }
+      reload();
+    }
   };
 </script>
 
-<div class="flex w-full p-4">
-  <Button class="ml-auto" href="/deployments/postgres/create">
-    <Plus />
-    Create
-  </Button>
-  {#each deployments as deployment}
-    <div class="flex items-center gap-2">
-      <span>{deployment.name}</span>
-      <Button variant="outline" onclick={() => remove(deployment.uuid)}>
-        Delete
-      </Button>
+<div class="flex flex-col gap-4">
+  <div class="flex w-full px-4 pt-4">
+    <Button class="ml-auto" href="/deployments/postgres/create">
+      <Plus />
+      Create
+    </Button>
+  </div>
+  <ScrollArea.ScrollArea class="mx-4 mb-4 min-h-0 grow">
+    <div
+      class="grid size-full auto-rows-min grid-cols-[repeat(auto-fill,minmax(14rem,1fr))] gap-2"
+    >
+      {#each deployments as deployment}
+        <div class="flex w-80 rounded-md border p-4">
+          <div class="flex flex-col">
+            <p class="font-bold">{deployment.name}</p>
+            <p class="text-muted-foreground">Postgres Deployment</p>
+          </div>
+          <Button
+            variant="destructive"
+            class="ml-auto cursor-pointer"
+            onclick={() => {
+              toDeleteName = deployment.name;
+              toDeleteUUID = deployment.uuid;
+              deleteOpen = true;
+            }}
+          >
+            Delete
+          </Button>
+        </div>
+      {:else}
+        <div
+          class="col-span-full flex flex-col items-center justify-center gap-2 py-8"
+        >
+          <p class="text-muted-foreground">No deployments found.</p>
+        </div>
+      {/each}
     </div>
-  {/each}
+  </ScrollArea.ScrollArea>
 </div>
+<FormDialog
+  title="Delete Deployment"
+  description={`Are you sure you want to delete the deployment "${toDeleteName}"? This action cannot be undone.`}
+  confirm="Delete"
+  confirmVariant="destructive"
+  onsubmit={deleteConfirm}
+  bind:open={deleteOpen}
+  schema={deleteDeployment}
+/>
